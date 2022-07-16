8bca07ec66412b3f18a9604fe18ddf35205f305f
slicetopieces <markovich1990mark@gmail.com>
2022-07-16 16:24:58 +0300

    Created new branch with new component

---
+++ src/components/UserForm.vue
@@ -0,0 +1,23 @@
+<template>
+</template>
+
+<script>
+export default {
+    name: 'UserForm',
+    data() {
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+input {
+  width: 400px;
+}
+.form-fields {
+  margin-right: 50px;
+  margin-bottom: 50px;
+}
+</style>
